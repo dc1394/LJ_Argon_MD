7a8c5dbc6eb0b063094a212a3508323061101a43
Dedeko <igrek@mvg.biglobe.ne.jp>
2015-09-07 18:31:09 +0900

    細かい修正

--- utility/utility.h
+++ utility/utility.h
@@ -10,52 +10,11 @@
 
 #pragma once
 
-#include <array>                // for std::array
-#include <cstdint>              // for std::int32_t
-#include <memory>               // for std::unique_ptr
-#include <string>               // for std::wstring
 #include <boost/cast.hpp>       // for boost::numeric_cast
 #include <boost/optional.hpp>   // for boost::optional
 #include <boost/utility.hpp>    // for boost::checked_delete
 
 namespace utility {
-    //! A function.
-    /*!
-        マルチバイト文字列をワイド文字列に変換する
-        \param mbs マルチバイト文字列
-        \param codeMulti マルチバイト文字列のエンコード（デフォルト=932）
-        \return ワイド文字列
-    */
-    std::wstring my_mbstowcs(std::string const & mbs, std::int32_t codeMulti = 932);
-
-    //! A function.
-    /*!
-        ファイルを開く
-        \return ファイル名
-    */
-    std::string myOpenFile();
-
-    //! A function.
-    /*!
-        ワイド文字列をマルチバイト文字列に変換する
-        \param wcs ワイド文字列
-        \param codeMulti マルチバイト文字列のエンコード（デフォルト=932）
-        \return マルチバイト文字列
-    */
-    std::string my_wcstombs(std::array<wchar_t, MAX_PATH> const & wcs, std::int32_t codeMulti = 932);
-
-    //! A function.
-    /*!
-        ファイル選択ダイアログを開く
-        \param hWnd ウィンドウハンドル
-        \param filepath ファイルのパス
-        \param filename ファイル名
-        \param title ファイル選択ダイアログのタイトル
-        \param defextension デフォルトのファイルの種類
-        \return ファイル選択ダイアログの戻り値
-    */
-    BOOL showFileDialog(HWND hWnd, wchar_t * filepath, wchar_t * filename, wchar_t * title, wchar_t * defextension);
-
     template <typename T>
     //! A function.
     /*!
